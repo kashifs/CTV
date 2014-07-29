d066439da4d09e44fe2e8aa36bc66f205f8430ad
Kashif Smith <smithkashif@gmail.com>
2014-07-29 11:59:45 -0700

    add senors.java to git

---
+++ CTV/src/Sensors.java
@@ -0,0 +1,33 @@
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ * @author modified from code found at: www.codejava.net
+ *
+ */
+public class Sensors {
+
+	private JCheckBox biologic = new JCheckBox("Biologic");
+	private JCheckBox chemical_gas = new JCheckBox("Chemical & Gas");
+	private JCheckBox motion = new JCheckBox("Motion");
+	private JCheckBox particle_radiation = new JCheckBox("Particle & Radiation");
+
+	public Sensors() {
+		JCheckBox[] categories = { biologic, chemical_gas, motion, particle_radiation };
+
+		int n = JOptionPane.showConfirmDialog(null, categories, "Sensors",
+				JOptionPane.OK_CANCEL_OPTION);
+
+		for (int i = 0; i < categories.length; i++) {
+			JCheckBox temp = (JCheckBox) categories[i];
+			if (temp.isSelected())
+				System.out.println(temp.getActionCommand());
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new Sensors();
+	}
+}
